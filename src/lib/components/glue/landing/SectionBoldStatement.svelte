bf1b73e191d672562adf8f8d4c9eb7d19d97b98a
Kevin Zhen <kz72@cornell.edu>
2023-09-12 20:22:46 -0400

    added animations
    - added fade-in animation from Saos

--- src/lib/components/glue/landing/SectionBoldStatement.svelte
+++ src/lib/components/glue/landing/SectionBoldStatement.svelte
@@ -8,32 +8,35 @@
 	export let desc2: string = '';
 	export let linkLabel: string = '';
 	export let linkHref: string = '';
+	import Saos from 'saos';
 </script>
 
 <section class="relative min-h-[60vh] overflow-hidden px-8 py-16 text-center md:pt-48">
 	<RadialGradient />
-	<p class="text-lg font-bold text-primary">{overline}</p>
-	<div class="flex justify-center">
-		<h2 class="mt-4 max-w-3xl text-4xl font-extrabold !leading-snug tracking-tight sm:text-5xl">
-			{heading}
-		</h2>
-	</div>
-	{#if desc}
-		<p
-			class="text-md mx-auto mt-10 max-w-xl font-normal leading-relaxed text-base-content/70 md:text-lg">
-			{desc}
-		</p>
-	{/if}
-	{#if desc2}
-		<p
-			class="text-md mx-auto mt-4 max-w-xl font-normal leading-relaxed text-base-content/70 md:text-lg">
-			{desc2}
-		</p>
-	{/if}
-	{#if linkLabel}
-		<a class="btn-primary btn mt-10 gap-1" href={linkHref}>
-			{linkLabel}
-			<span class="text-xl"><IconRightArrowLong /></span>
-		</a>
-	{/if}
+	<Saos animation={'fade-in 0.5s cubic-bezier(0.470, 0.000, 0.745, 0.715) both'} once={true}>
+		<p class="text-primary text-lg font-bold">{overline}</p>
+		<div class="flex justify-center">
+			<h2 class="mt-4 max-w-3xl text-4xl font-extrabold !leading-snug tracking-tight sm:text-5xl">
+				{heading}
+			</h2>
+		</div>
+		{#if desc}
+			<p
+				class="text-md text-base-content/70 mx-auto mt-10 max-w-xl font-normal leading-relaxed md:text-lg">
+				{desc}
+			</p>
+		{/if}
+		{#if desc2}
+			<p
+				class="text-md text-base-content/70 mx-auto mt-4 max-w-xl font-normal leading-relaxed md:text-lg">
+				{desc2}
+			</p>
+		{/if}
+		{#if linkLabel}
+			<a class="btn-primary btn mt-10 gap-1" href={linkHref}>
+				{linkLabel}
+				<span class="text-xl"><IconRightArrowLong /></span>
+			</a>
+		{/if}
+	</Saos>
 </section>
