52c12fd9ffa5f1b41c5fe5f1398d6aad8157dcd4
Jay Joo <jj534@cornell.edu>
2023-11-29 14:21:45 -0500

    group profiles by cohort

--- src/routes/team/+page.svelte
+++ src/routes/team/+page.svelte
@@ -4,7 +4,7 @@
 	import SectionHero from '$lib/components/glue/landing/SectionHero.svelte';
 	import groupPhotoSrc from '$lib/assets/group-photo-md.png';
 
-	$: ({ profiles } = $page.data);
+	$: ({ profileGroups } = $page.data);
 </script>
 
 <PageContainer>
@@ -15,18 +15,19 @@
 		descBottomLine="passionate about building performant web applications"
 		mainImageSrc={groupPhotoSrc} />
 
-	<h2 class="text-4xl font-extrabold">Members</h2>
-	<div class="mt-6 flex flex-wrap justify-start">
-		{#each profiles as profile}
-			<div class="w-1/2 py-6 pr-6 md:w-1/3 lg:w-1/4">
-				<img
-					class="h-[280px] w-full overflow-hidden rounded-lg object-cover "
-					src={profile?.avatarUrl}
-					loading="lazy" />
-				<p class="mt-3 text-lg font-semibold">{profile?.firstName} {profile?.lastName}</p>
-				<p class="text-sm text-base-content/80">{profile?.title}</p>
-			</div>
-		{/each}
-	</div>
-	</div>
+	{#each profileGroups as profileGroup}
+		<h2 class="mt-16 text-4xl font-extrabold">{profileGroup?.name}</h2>
+		<div class="mt-4 flex flex-wrap justify-start">
+			{#each profileGroup?.profiles as profile}
+				<div class="w-1/2 py-6 pr-6 md:w-1/3 lg:w-1/4">
+					<img
+						class="h-[280px] w-full overflow-hidden rounded-lg object-cover "
+						src={profile?.avatarUrl}
+						loading="lazy" />
+					<p class="mt-3 text-lg font-semibold">{profile?.firstName} {profile?.lastName}</p>
+					<p class="text-sm text-base-content/80">{profile?.title}</p>
+				</div>
+			{/each}
+		</div>
+	{/each}
 </PageContainer>
--- src/routes/team/+page.ts
+++ src/routes/team/+page.ts
@@ -1,11 +1,24 @@
 import type { Load } from '@sveltejs/kit';
 
 export const load: Load = async ({ parent }) => {
-	const fetchProfiles = async () => {
+	const fetchProfileGroups = async () => {
 		const { supabase } = await parent();
 		const { data: profiles } = await supabase.from('profiles').select('*');
-		return profiles;
+		const profileGroupDict = {};
+		for (const profile of profiles) {
+			const { cohort } = profile;
+			if (cohort in profileGroupDict) {
+				profileGroupDict[cohort]?.push(profile);
+			} else {
+				profileGroupDict[cohort] = [profile];
+			}
+		}
+		const profileGroups = Object.entries(profileGroupDict)?.map(([cohort, profiles]) => ({
+			name: cohort,
+			profiles
+		}));
+		return profileGroups;
 	};
 
-	return { profiles: fetchProfiles() };
+	return { profileGroups: fetchProfileGroups() };
 };
