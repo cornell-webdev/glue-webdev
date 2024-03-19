b58ca73af2578608d3362b02fc3eca01983c94a7
Jay Joo <jj534@cornell.edu>
2024-03-19 19:06:31 -0400

    implement slide up lamp feature

---
+++ src/lib/components/Product.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+	import IntersectionObserver from 'svelte-intersection-observer';
+	import cornletHomeSrc from '$lib/assets/cornlet-home.jpg';
+	import IconOpenInNew from '$lib/icons/glue/IconOpenInNew.svelte';
+	import Saos from 'saos';
+
+	let element;
+	let intersecting;
+	let color = '179,27,27';
+	let colorDark = '144,49,49';
+</script>
+
+<div class="my-[6rem]">
+	<section
+		class="{intersecting
+			? 'is-visible before:opacity-100 before:[transform:rotate(180deg)_scale(3)]'
+			: 'before:rotate-180 before:opacity-40'}
+    is-visible after:bg-[radial-gradient(ellipse_100%_40%_at_50%_60%,rgba(var(--feature-color),0.1),transparent) relative flex flex-col items-center
+  overflow-x-clip before:pointer-events-none before:absolute before:h-[400px] before:w-full
+  before:bg-[conic-gradient(from_90deg_at_80%_50%,#000212,rgb(var(--feature-color-dark))),conic-gradient(from_270deg_at_20%_50%,rgb(var(--feature-color-dark)),#000212)] before:bg-no-repeat
+  before:opacity-100 before:transition-[transform,opacity] before:duration-500 before:ease-in
+  before:[mask:radial-gradient(100%_50%_at_center_center,_black,_transparent)] before:[background-size:50%_100%,50%_100%]
+  before:[background-position:1%_0%,99%_0%] after:pointer-events-none after:absolute after:inset-0"
+		style="--feature-color: {color}; --feature-color-dark: {colorDark};">
+		<div class="z-10 mt-[6rem] mb-16 flex w-full flex-col items-center">
+			<div
+				class="{intersecting
+					? 'translate-y-0'
+					: 'translate-y-[3rem]'} [transition:transform_1000ms_cubic-bezier(0.3,_1.17,_0.55,_0.99)_0s]">
+				<h2
+					class="pt-[12rem] text-center text-4xl font-extrabold !leading-snug tracking-tight md:text-6xl">
+					Subleting has
+					<br />
+					never been easier
+				</h2>
+
+				<div class="mt-6 flex justify-center">
+					<a href="https://www.cornlet.com" target="_blank" rel="noreferrer">
+						<button class="btn-secondary btn">Visit Cornlet <IconOpenInNew /></button>
+					</a>
+				</div>
+			</div>
+
+			<IntersectionObserver {element} bind:intersecting threshold={0.5}>
+				<div bind:this={element} />
+			</IntersectionObserver>
+
+			<div class="mt-20 flex w-full justify-center">
+				<img class="w-[80%] rounded-xl" src={cornletHomeSrc} />
+			</div>
+
+			<Saos animation={'fade-in 0.5s cubic-bezier(0.470, 0.000, 0.745, 0.715) both'} once={false}>
+				<div class="mt-48 max-w-lg px-6">
+					<p
+						class="mx-auto mt-10 max-w-xl text-center text-lg font-normal leading-relaxed text-base-content/70 md:text-lg">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente velit mollitia
+						repellendus illum pariatur atque repellat qui, cum fugit iste.
+					</p>
+
+					<p
+						class="mx-auto mt-10 max-w-xl text-center text-lg font-normal leading-relaxed text-base-content/70 md:text-lg">
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente velit mollitia
+						repellendus illum pariatur atque repellat qui, cum fugit iste.
+					</p>
+				</div>
+			</Saos>
+		</div>
+	</section>
+</div>
--- src/routes/project/+page.svelte
+++ src/routes/project/+page.svelte
@@ -3,41 +3,16 @@
 	import PageContainer from '$lib/components/glue/PageContainer.svelte';
 	import SectionBoldStatement from '$lib/components/glue/landing/SectionBoldStatement.svelte';
 	import Saos from 'saos';
+	import Product from '$lib/components/Product.svelte';
 
 	$: ({ projects } = $page.data);
 </script>
 
-<PageContainer title="Projects">
+<PageContainer title="Projects" isHoriPadding={false}>
 	<SectionBoldStatement
 		overline="Our projects"
 		heading="We've built so many things out of interest and passion"
 		desc="Our projects are either solving critical problems for the Cornell community or were a fun experience to build something cool " />
-	<Saos animation={'fade-in 0.5s cubic-bezier(0.470, 0.000, 0.745, 0.715) both'} once={true}>
-		<div class="mb-32 flex justify-center">
-			<div class="w-full max-w-3xl">
-				<h1 class="text-4xl font-extrabold">Our projects</h1>
-				<p class="mt-8 text-base-content/80">
-					Some of our projects have been discontinued, and that's totally okay! We like to emphasize
-					building whatever interests you quickly, deploying, and seeing how the market reacts.
-				</p>
-				<p class="mt-3 text-base-content/80">
-					Even if the market responds poorly, there's so much to be learned from a "failed" project.
-					These lessons often equip us with valuable insights that help propel the next project.
-				</p>
-				<div class="mt-12">
-					{#each projects as project (project.id)}
-						<div class="my-6 flex">
-							<img class="w-40 rounded-xl bg-base-content/10" src={project?.imgUrl} />
-							<div class="ml-6">
-								<h3 class="text-3xl font-extrabold">{project?.name}</h3>
-								<p class="mt-2 whitespace-pre-wrap leading-relaxed text-base-content/80">
-									{project?.description}
-								</p>
-							</div>
-						</div>
-					{/each}
-				</div>
-			</div>
-		</div>
-	</Saos>
+
+	<Product />
 </PageContainer>
